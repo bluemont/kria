6c0fdd4cd56740aa060abc0e3426a70fde2f8fef
David James <david@bluemontlabs.com>
2014-03-13 15:22:35 -0400

    demonstrate use of Yokozuna dynamic fields
    From the default schema:
    
    <dynamicField name="*_ss"
      type="string"
      indexed="true"
      stored="true"
      multiValued="true"/>

--- doc/examples/search.clj
+++ doc/examples/search.clj
@@ -1,8 +1,8 @@
 (defn cb-fn [p] (fn [asc e a] (deliver p [asc e a])))
 (defn conn-cb [asc e a] (println (if e e "connected")))
 (def conn (client/connect nil "127.0.0.1" 8087 conn-cb))
-(def b (conv/byte-string<-utf8-string "B-503"))
-(def idx "I-503")
+(def b (conv/byte-string<-utf8-string "B-504"))
+(def idx "I-504")
 
 (def p (promise))
 (index/put conn idx {} (cb-fn p))
@@ -17,23 +17,22 @@
   (str "{\"" jk "\" : \"" jv "\"}"))
 
 (def k (conv/byte-string<-utf8-string "Key-1"))
-(def v {:value (conv/byte-string<-utf8-string (json "word" "ocean"))
+(def v {:value (conv/byte-string<-utf8-string (json "word_ss" "ocean"))
         :content-type "application/json"})
 (object/put conn b k v {} result-cb)
 
 (def k (conv/byte-string<-utf8-string "Key-2"))
-(def v {:value (conv/byte-string<-utf8-string (json "word" "stream"))
+(def v {:value (conv/byte-string<-utf8-string (json "word_ss" "stream"))
         :content-type "application/json"})
 (object/put conn b k v {} result-cb)
 
 (def k (conv/byte-string<-utf8-string "Key-3"))
-(def v {:value (conv/byte-string<-utf8-string (json "word" "river"))
+(def v {:value (conv/byte-string<-utf8-string (json "word_ss" "river"))
         :content-type "application/json"})
 (object/put conn b k v {} result-cb)
 
-(def q (conv/byte-string<-utf8-string "word:*"))
-; (search/search conn q idx {} (cb-fn p))
+(def q (conv/byte-string<-utf8-string "word_ss:*"))
 (def p (promise))
-(search/search conn q idx {:fl ["*"]} (cb-fn p))
+(search/search conn q idx {} (cb-fn p))
 (println @p)
 
