9f98fa002bda24579566c1ce16357ce50220c928
David James <david@bluemontlabs.com>
2014-03-19 20:14:44 -0400

    use Clojure 1.6.0-RC1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.6.0-RC1"]
                  [com.google.protobuf/protobuf-java "2.5.0"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
