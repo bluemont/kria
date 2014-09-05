ab8e7b24cc873038fd33961bd0b7f12cd21ca1e3
David James <david@bluemontlabs.com>
2014-09-05 12:34:59 -0400

    version 0.1.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kria "0.1.15-SNAPSHOT"
+(defproject kria "0.1.15"
   :description "A Clojure client library for Riak 2.0.0. Uses Java 7 NIO.2."
   :url "https://github.com/bluemont/kria"
   :license {:name "Eclipse Public License"
