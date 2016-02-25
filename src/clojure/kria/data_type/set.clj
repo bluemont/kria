93ed52fccc0bf2fa3290388b0725948bf775f57c
Milt Reder <milt@anestuary.com>
2016-02-25 15:16:13 -0500

    dt ops -> clj pb

--- src/clojure/kria/data_type/set.clj
+++ src/clojure/kria/data_type/set.clj
@@ -1,25 +1,15 @@
 (ns kria.data-type.set
   (:refer-clojure :exclude [get update])
   (:require
-   [kria.pb.dt.op :refer [DtOp->pb]]
-   [kria.pb.dt.set :refer [SetOp->pb]]
-   [kria.data-type :as dt])
-  (:import
-   [com.basho.riak.protobuf
-    RiakDtPB$DtValue]))
+   [kria.data-type :as dt]))
 
 (set! *warn-on-reflection* true)
-
-(defn ->DtSetOp
-  [m]
-  (DtOp->pb
-   {:set-op (SetOp->pb m)}))
 
 (defn update
   "Update a set in a bucket."
   [asc b t k adds removes opts cb]
   (dt/set asc b t k
-          (->DtSetOp {:adds adds :removes removes})
+          {:set-op {:adds adds :removes removes}}
           opts cb))
 
 (defn get
@@ -29,4 +19,4 @@
           (fn [asc e a]
             (if e
               (cb asc e nil)
-              (cb asc e (.getSetValueList ^RiakDtPB$DtValue (:value a)))))))
+              (cb asc e (:set-value (:value a)))))))
--- src/clojure/kria/pb/dt/fetch.clj
+++ src/clojure/kria/pb/dt/fetch.clj
@@ -1,4 +1,6 @@
 (ns kria.pb.dt.fetch
+  (:require
+   [flatland.protobuf.core :as pb])
   (:import
    [com.basho.riak.protobuf
     RiakDtPB$DtFetchReq
@@ -6,64 +8,18 @@
 
 (set! *warn-on-reflection* true)
 
-(defrecord DtFetchReq
-           [bucket          ; required bytes
-            key             ; required bytes
-            type            ; required bytes
-            r               ; optional uint32
-            pr              ; optional uint32
-            basic-quorum    ; optional bool
-            notfound-ok     ; optional bool
-            timeout         ; optional uint32
-            sloppy-quorum   ; optional bool
-            n-val           ; optional uint32
-            include-context ; optional bool
-            ])
-
-(defn ^RiakDtPB$DtFetchReq DtFetchReq->pb
-  [m]
-  (let [b (RiakDtPB$DtFetchReq/newBuilder)]
-    (let [x (:bucket m)]
-      (.setBucket b x))
-    (let [x (:key m)]
-      (.setKey b x))
-    (let [x (:type m)]
-      (.setType b x))
-    (if-let [x (:r m)]
-      (.setR b x))
-    (if-let [x (:pr m)]
-      (.setPr b x))
-    (if-let [x (:basic-quorum m)]
-      (.setBasicQuorum b x))
-    (if-let [x (:notfound-ok m)]
-      (.setNotfoundOk b x))
-    (if-let [x (:timeout m)]
-      (.setTimeout b x))
-    (if-let [x (:sloppy-quorum m)]
-      (.setSloppyQuorum b x))
-    (if-let [x (:n-val m)]
-      (.setNVal b x))
-    (if-let [x (:include-context m)]
-      (.setIncludeContext b x))
-    (.build b)))
+(def DtFetchReq
+  (pb/protodef RiakDtPB$DtFetchReq))
 
 (defn DtFetchReq->bytes
   [m]
-  (.toByteArray (DtFetchReq->pb m)))
+  (pb/protobuf-dump
+   (pb/protobuf DtFetchReq
+                m)))
 
-(defrecord DtFetchResp
-           [context ; optional bytes
-            type    ; required DataType
-            value   ; optional DtValue
-            ])
-
-(defn pb->DtFetchResp
-  [^RiakDtPB$DtFetchResp pb]
-  (->DtFetchResp
-   (.getContext pb)
-   (.getType pb)
-   (.getValue pb)))
+(def DtFetchResp
+  (pb/protodef RiakDtPB$DtFetchResp))
 
 (defn bytes->DtFetchResp
   [^bytes x]
-  (pb->DtFetchResp (RiakDtPB$DtFetchResp/parseFrom x)))
+  (pb/protobuf-load DtFetchResp x))
