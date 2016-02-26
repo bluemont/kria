7ce0a65ab3cc6095cc593b36dc6e66f1f2cfc46e
Milt Reder <milt@anestuary.com>
2016-02-26 15:15:19 -0500

    updated examples

--- doc/examples/search.clj
+++ doc/examples/search.clj
@@ -2,7 +2,8 @@
 (defn conn-cb [asc e a] (println (if e e "connected")))
 (def conn (client/connect "127.0.0.1" 8087 conn-cb))
 (def b (conv/byte-string<-utf8-string "B-504"))
-(def idx "I-504")
+(def idx (conv/byte-string<-utf8-string "I-504"))
+(def ctype (conv/byte-string<-utf8-string "application/json"))
 
 (def p (promise))
 (index/put conn idx {} (cb-fn p))
@@ -18,21 +19,20 @@
 
 (def k (conv/byte-string<-utf8-string "Key-1"))
 (def v {:value (conv/byte-string<-utf8-string (json "word_ss" "ocean"))
-        :content-type "application/json"})
+        :content-type ctype})
 (object/put conn b k v {} result-cb)
 
 (def k (conv/byte-string<-utf8-string "Key-2"))
 (def v {:value (conv/byte-string<-utf8-string (json "word_ss" "stream"))
-        :content-type "application/json"})
+        :content-type ctype})
 (object/put conn b k v {} result-cb)
 
 (def k (conv/byte-string<-utf8-string "Key-3"))
 (def v {:value (conv/byte-string<-utf8-string (json "word_ss" "river"))
-        :content-type "application/json"})
+        :content-type ctype})
 (object/put conn b k v {} result-cb)
 
 (def q (conv/byte-string<-utf8-string "word_ss:*"))
 (def p (promise))
 (search/search conn idx q {} (cb-fn p))
 (println @p)
-
