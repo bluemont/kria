2162eec1e3d34810a2d12d9634128ad06edbbf9e
Milt Reder <milt@anestuary.com>
2016-02-26 13:06:49 -0500

    updated deps to latest possible

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.google.protobuf/protobuf-java "2.6.1"]
                  [com.basho.riak.protobuf/riak-pb "2.0.0.16"]
-                 [org.beatlevic/protobuf "0.8.2"]]
+                 [org.clojars.ghaskins/protobuf "0.1"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :profiles {:dev
              {:source-paths ["dev"]
-              :dependencies [[org.clojure/tools.namespace "0.2.10"]
+              :dependencies [[org.clojure/tools.namespace "0.2.11"]
                              [org.clojure/data.json "0.2.6"]]}})
